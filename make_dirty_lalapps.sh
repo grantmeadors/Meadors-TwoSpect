c7143025cdc344bd1814588f37829237ab7a09b0
Grant Meadors <grant.meadors@ligo.org>
2013-09-07 01:19:28 +0200

    Made a simpler makefile just to make LALApps, seeing whether it will work. Grant David Meadors, 02013-09-06.

---
+++ make_dirty_lalapps.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+LSCSOFT_SRCDIR=${LSCSOFT_SRCDIR:-"${HOME}/src/lscsoft/"}
+LSCSOFT_ROOTDIR=${LSCSOFT_ROOTDIR:-"${HOME}/master"}
+#LAL_PREFIX=${LAL_PREFIX:-"${LSCSOFT_ROOTDIR}/opt/lscsoft/lal"}
+#cd ${LSCSOFT_SRCDIR}/lalsuite/lal
+#CFLAGS="-O3 -msse -msse2"
+#./00boot
+#./configure --prefix=${LAL_PREFIX} --disable-gcc-flags --disable-debug
+#make -j3 install
+
+#LALPULSAR_PREFIX=${LALPULSAR_PREFIX:-"${LSCSOFT_ROOTDIR}/opt/lscsoft/lalpulsar"}
+#cd ${LSCSOFT_SRCDIR}/lalsuite/lalpulsar
+#./00boot
+#./configure --prefix=${LALPULSAR_PREFIX} --disable-gcc-flags --disable-debug
+#make -j3 install
+
+source ${LSCSOFT_ROOTDIR}/etc/lscsoftrc
+LALAPPS_PREFIX=${LALAPPS_PREFIX:-"${LSCSOFT_ROOTDIR}/opt/lscsoft/lalapps"}
+cd ${LSCSOFT_SRCDIR}/lalsuite/lalapps
+#./00boot
+#./configure --prefix=${LALAPPS_PREFIX} --enable-condor --disable-gcc-flags --disable-debug
+#make -j3 install
+./configure --enable-condor
+make
+make install
+
