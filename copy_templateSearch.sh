ba4dae080326a77a262084f45fbea867992b81f6
Grant Meadors <grant.meadors@ligo.org>
2013-11-14 18:31:36 +0100

    Added the script to copy work on template test to the source directory

---
+++ copy_templateSearch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp templates.c /home/grant.meadors/src/lscsoft/lalsuite/lalapps/src/pulsar/TwoSpect
+cp templates.h /home/grant.meadors/src/lscsoft/lalsuite/lalapps/src/pulsar/TwoSpect
+cp TwoSpect.c /home/grant.meadors/src/lscsoft/lalsuite/lalapps/src/pulsar/TwoSpect
