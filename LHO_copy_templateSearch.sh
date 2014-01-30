3dcbb45651dddc15b7234e1db248c66aeeaf0a9c
Grant Meadors <grant.meadors@ligo.org>
2014-01-30 13:12:20 -0800

    Adding commands for copying files specific to LHO into the git repository; these helperscripts are necessarily different due to the different layout of the LDG clusters compared to Atlas. Grant David Meadors, 02014-01-30 (JD 24566882)

---
+++ LHO_copy_templateSearch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp templates.c /usr1/gmeadors/src/lscsoft/lalsuite/lalapps/src/pulsar/TwoSpect
+cp templates.h /usr1/gmeadors/src/lscsoft/lalsuite/lalapps/src/pulsar/TwoSpect
+cp TwoSpect.c /usr1/gmeadors/src/lscsoft/lalsuite/lalapps/src/pulsar/TwoSpect
---
+++ LHO_make_dirty_lalapps.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+LSCSOFT_SRCDIR=${LSCSOFT_SRCDIR:-"${HOME}/src/lscsoft/"}
+LSCSOFT_ROOTDIR=${LSCSOFT_ROOTDIR:-"${HOME}/master"}
+#LAL_PREFIX=${LAL_PREFIX:-"${LSCSOFT_ROOTDIR}/opt/lscsoft/lal"}
+#cd ${LSCSOFT_SRCDIR}/lalsuite/lal
+#CFLAGS="-O3 -msse -msse2"
+#./00boot
+#./configure --prefix=${LAL_PREFIX} --disable-gcc-flags --disable-debug
+#make -j3 install
+
+#LALPULSAR_PREFIX=${LALPULSAR_PREFIX:-"${LSCSOFT_ROOTDIR}/opt/lscsoft/lalpulsar"}
+#cd ${LSCSOFT_SRCDIR}/lalsuite/lalpulsar
+#./00boot
+#./configure --prefix=${LALPULSAR_PREFIX} --disable-gcc-flags --disable-debug
+#make -j3 install
+
+source ${LSCSOFT_ROOTDIR}/etc/lscsoftrc
+LALAPPS_PREFIX=${LALAPPS_PREFIX:-"${LSCSOFT_ROOTDIR}/opt/lscsoft/lalapps"}
+#cd ${LSCSOFT_SRCDIR}/lalsuite/lalapps
+#./00boot
+#./configure --prefix=${LALAPPS_PREFIX} --enable-condor --disable-gcc-flags --disable-debug
+#make -j3 install
+
+#./configure --prefix=${LALAPPS_PREFIX} --enable-condor
+#make
+#make install
+
+cd /usr1/gmeadors/src/lscsoft/lalsuite/lalapps/src/pulsar/TwoSpect/
+#./configure --prefix=${LALAPPS_PREFIX} --enable-condor
+make
+make install
+
