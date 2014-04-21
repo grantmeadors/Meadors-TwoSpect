7462da75235abea9359e54b384de7d21e736ccff
Grant Meadors <grant.meadors@ligo.org>
2014-04-21 13:10:23 -0700

    Added changes for different Tsft to the output file paths in compareCandidates. Grant David Meadors, 02014-04-21 (JD 2456769)

--- helperprograms/compareCandidates.c
+++ helperprograms/compareCandidates.c
@@ -51,10 +51,10 @@ INT4 main(void) {
    //const char *outfile4 = "/Users/evgoet/Documents/MATLAB/pulsar/S6/500-520HzCandidates_output3.dat";
    const char *infile1 = "/home/gmeadors/ScoX1/candidates/H1open/ProbabilityMinimaH1.txt";
    const char *infile2 = "/home/gmeadors/ScoX1/candidates/L1open/ProbabilityMinimaL1.txt";
-   const char *outfile1 = "/home/gmeadors/ScoX1/candidates/outputH1L1open/Candidates_output1.dat";
-   const char *outfile2 = "/home/gmeadors/ScoX1/candidates/outputH1L1open/Candidates_output2.dat";
-   const char *outfile3 = "/home/gmeadors/ScoX1/candidates/outputH1L1open/Candidates_final_H1L1.dat";
-   const char *outfile4 = "/home/gmeadors/ScoX1/candidates/outputH1L1open/Candidates_output3.dat";
+   const char *outfile1 = "/home/gmeadors/ScoX1/candidates/360s/outputH1L1open/Candidates_output1.dat";
+   const char *outfile2 = "/home/gmeadors/ScoX1/candidates/360s/outputH1L1open/Candidates_output2.dat";
+   const char *outfile3 = "/home/gmeadors/ScoX1/candidates/360s/outputH1L1open/Candidates_final_H1L1.dat";
+   const char *outfile4 = "/home/gmeadors/ScoX1/candidates/360s/outputH1L1open/Candidates_output3.dat";
 
    XLAL_CHECK( (H1CANDS = fopen(infile1,"r")) != NULL, XLAL_EIO, "Can't fopen %s", infile1 );
 
