e8e363d27042ed84124d82c9329504d6bd79945c
Grant Meadors <grant.meadors@ligo.org>
2014-05-14 21:10:54 -0700

    Will copy to this other directory, see if works

--- LHO_copy_executable.sh
+++ LHO_copy_executable.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cp /home/gmeadors/master/opt/lscsoft/lalapps/bin/lalapps_TwoSpect /home/gmeadors/TwoSpect/dev/bin4
+cp /home/gmeadors/master/opt/lscsoft/lalapps/bin/lalapps_TwoSpect /home/gmeadors/TwoSpect/dev/bin5
