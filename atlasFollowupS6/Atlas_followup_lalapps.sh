b0ef4957808322b4c017b608a0473b504ecad043
Grant Meadors <grant.meadors@ligo.org>
2015-10-15 13:59:07 +0200

    Wrote a compiler script that should compile a new, patched version of TwoSpect suited to doing follow-up analyses on S6, and can be in preparation for O1

---
+++ atlasFollowupS6/Atlas_followup_lalapps.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# For clean compiling TwoSpect and ALL of LALapps
+# In the version used for S6 followup
+# Grant David Meadors
+# 02015-10-15 (JD 2457311)
+# g r a n t . m e a d o r s @ a e i . m p g . d e
+
+LALSUITE_SRCDIR=${HOME}/dev/followupS6/LALApps/src
+LALSUITE_PREFIX=${HOME}/dev/followupS6/LALApps/opt/lalsuite
+cd ${LALSUITE_SRCDIR}/lalsuite
+./00boot
+CFLAGS="-O3 -msse -msse2 -mavx"
+./configure --prefix=${LALSUITE_PREFIX} --with-simd
+make -j
+make install
+
+mkdir ${HOME}/dev/followupS6/LALApps/etc
+echo "source ${LALSUITE_PREFIX}/etc/lal-user-env.\`echo \$0 | tr -c -d csh\`" > ${HOME}/dev/followupS6/LALApps/etc/lalsuiterc
+source ${HOME}/dev/followupS6/etc/lalsuiterc
