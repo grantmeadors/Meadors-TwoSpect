54cf898faae069a713290676be586b260f33c5a3
Grant David Meadors <grant.meadors@ligo.org>
2016-01-10 22:00:07 +0100

    New compile script to add Doxygenation

---
+++ AEI_doxygen.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# For compiling Lalapps with Doxygen
+# Grant David Meadors
+# 02016-01-10(JD 2457398)
+# g r a n t . m e a d o r s @ a e i . m p g . d e
+
+LALSUITE_SRCDIR=${HOME}/Documents/LALApps/src
+LALSUITE_PREFIX=${HOME}/Documents/LALApps/opt/lalsuite
+cd ${LALSUITE_SRCDIR}/lalsuite
+./00boot
+CFLAGS="-O3 -msse -msse2 -mavx"
+./configure --prefix=${LALSUITE_PREFIX} --with-simd --enable-doxygen
+make -j2
+make install
+
+mkdir ${HOME}/Documents/LALApps/etc
+echo "source ${LALSUITE_PREFIX}/etc/lal-user-env.\`echo \$0 | tr -c -d csh\`" > ${HOME}/Documents/LALApps/etc/lalsuiterc
+source ${HOME}/Documents/LALApps/etc/lalsuiterc
+
+cd ${LALSUITE_SRCDIR}/lalsuite
+make html
+
