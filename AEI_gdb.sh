c41ad802b37b1226ec626fc89aacc93fa8ae52be
Grant David Meadors <grant.meadors@ligo.org>
2016-06-01 15:04:38 +0200

    Adding a command for building with the Gnu debugger, GDB.

---
+++ AEI_gdb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# For compiling Lalapps with Doxygen
+# Grant David Meadors
+# 02016-01-20 (JD 2457408)
+# g r a n t . m e a d o r s @ a e i . m p g . d e
+
+LALSUITE_SRCDIR=${HOME}/Documents/LALApps/src
+LALSUITE_PREFIX=${HOME}/Documents/LALApps/opt/lalsuite
+cd ${LALSUITE_SRCDIR}/lalsuite
+./00boot
+CFLAGS="-O3 -msse -msse2 -mavx -ggdb"
+./configure --prefix=${LALSUITE_PREFIX} --with-simd
+make -j2
+make install
+
+mkdir ${HOME}/Documents/LALApps/etc
+echo "source ${LALSUITE_PREFIX}/etc/lal-user-env.\`echo \$0 | tr -c -d csh\`" > ${HOME}/Documents/LALApps/etc/lalsuiterc
+source ${HOME}/Documents/LALApps/etc/lalsuiterc
+
